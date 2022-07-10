a5e2bd27f98724d50dbec1492ee4960a88c50c90
HuyNLQ <huy.nguyen@ascension6.com.au>
2022-07-10 12:51:06 +0700

    Disable update constant on Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     //
     public function index()
     {
-        $this->updateConstant();
+        // $this->updateConstant();
 
         return view('dashboard.index');
     }
